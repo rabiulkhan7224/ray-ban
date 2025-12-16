f9bda000d4a5d832f1f7e77a9d44560a82108afb
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-12-16 21:20:41 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,59 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div>
+      {/* header */}
+      <header>
+        <nav
+          className="flex items-center w-full bg-transparent fixed top-0 
+        justify-between p-4 z-30
+        "
+        >
+          {/* logo */}
+          <div className="">
+            <img
+              src="/ray-ban.svg"
+              alt="Logo"
+              className="bg-blend-color"
+              width={50}
+              height={50}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          </div>
+          {/* menu button */}
+          <Button className="menu-button">
+            <MenuIcon />
+          </Button>
+        </nav>
+      </header>
+
+      <main>
+        <section className="hero-section ">
+          {/* video container */}
+          <div className="video-container relative w-full h-full bg-black/90 ">
+            <video
+              src="/Hero_XL.mp4"
+              autoPlay
+              loop
+              muted
+              className="video-element"
+            />
+            {/* overlay text */}
+            <div className="overlay-text absolute top-2/3 left-1/4 transform -translate-x-1/2 -translate-y-1/2 text-left">
+              <h1 className="text-7xl font-semibold text-white mb-4">
+                Meta Ray-Ban <br />
+                Display
+              </h1>
+              <p className="text-lg text-white mb-8">
+                In-lens display. On-wrist control.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
-}
+};
+
+export default HomePage;
